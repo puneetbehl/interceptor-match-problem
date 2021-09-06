aef5b0a9986a0eeb4628286addbd822ed9837814
Puneet Behl <behlp@objectcomputing.com>
2021-09-06 17:50:23 +0530

    Custom error pages Tomcat

--- grails-app/controllers/com/synchr/interceptorDemo/UrlMappings.groovy
+++ grails-app/controllers/com/synchr/interceptorDemo/UrlMappings.groovy
@@ -10,7 +10,8 @@ class UrlMappings {
         }
 
         "/"(view:"/index")
-        "500"(view:'/error')
-        "404"(view:'/notFound')
+//        "500"(view:'/error')
+//        "404"(view:'/notFound')
+//        "400"(view:'/error')
     }
 }
